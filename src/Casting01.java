946f1933570dacc184dd2ca1ca24f92ca5791222
AounSyed <Aoungalaxy@gmail.com>
2025-03-10 14:51:19 -0400

    Completed assignment

--- src/Casting01.java
+++ src/Casting01.java
@@ -2,17 +2,22 @@ public class Casting01 {
     public static void main(String[] args) {
         //Declare and initialize a byte with a value of 128
         //Observe NetBeans' complaint
-
+        int x = 128;
         
         //Declare and initialize a short with a value of 128
         //Create a print statement that casts this short to a byte
-
+        short y = 128;
+        System.out.println((byte)(y));
         
         
         //Declare and initialize a byte with a value of 127
         //Add 1 to this variable and print it
         //Add 1 to this variable again and print it again
-
+        byte g = 127;
+        g++;
+        System.out.println(g);
+        g++;
+        System.out.println(g);
         
         
         
--- src/Parsing01.java
+++ src/Parsing01.java
@@ -1,18 +1,23 @@
 public class Parsing01 {
     public static void main(String[] args) {
         //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
-        
-        
-        
-        
+        String shirtPrice, taxRate, gibberish;
+        shirtPrice = "200";
+        taxRate = ".10";
+        gibberish = "wasd";
+
+
+
         //Parse shirtPrice and taxRate, and print the total tax
-        
-        
-        
+        double totalTax;
+        totalTax = Double.parseDouble(shirtPrice) * Double.parseDouble(taxRate);
+        System.out.println(totalTax);
+
+
         //Try to parse taxRate as an int
-        
+        //System.out.println(Integer.parseInt(taxRate));
         //Try to parse gibberish as an int
-        
+        //System.out.println(Integer.parseInt(gibberish));;
     }
     
 }
